489909b2fb87117a2c741950f4dc56e4804a5136
jvolcy <jerry.volcy@gmail.com>
2022-10-17 08:28:07 -0400

    cell merge added

--- Assets/Board/Board.cs
+++ Assets/Board/Board.cs
@@ -139,12 +139,6 @@ public class Board : MonoBehaviour
 
 
 
-    void AddBlockToColumn(int col, int power)
-    {
-        //Columns[col].AddBlock()
-    }
-
-
     void CreateBlock(int power)
     {
         GameObject obj = Instantiate(BlockPrefab); ////Columns[0].CreateBlock(power);
@@ -176,47 +170,31 @@ public class Board : MonoBehaviour
         CurrentBlock.transform.position = new Vector2(ColXPosition[CurrentColumn], CurrentBlock.transform.position.y);
     }
 
-    //    bool bFindingMergeables;
+
+    /// <summary>
+    /// Block trigger callback function
+    /// </summary>
+    /// <param name="bc"></param>
+    /// <param name="ObjCollidedWith"></param>
     public void BlockCallback(BlockController bc, GameObject ObjCollidedWith)
     {
-        //if (bc != CurrentBlock) return;
-        //        if (bFindingMergeables) return;
-        //        bFindingMergeables = true;
         Debug.Log("-->Callback: block " + bc.SerialNumber);
-
-        //assign to a column
-        //Columns[CurrentColumn].AddBlock(CurrentBlock, CurrentBlock.transform.position.y);
-
-        //detach the callback
-        //bc.callbackFunction = null; //FindMergeables;
 
         state = State.IDLE;
         BuildBoardMatrix();
         PrintBoard();
 
-        //check the last added block
-        //CheckBlockForMerge(Columns[CurrentColumn].Count - 1, CurrentColumn);
-
-        //now check all blocks
-
-        //FindMergeables();
-        //       bFindingMergeables = false;
+        //merge blocks as necessary
+        FindMergeables();
     }
- 
+
+
     void SetCurrentBlockGravityScale(float gravityScale)
     {
         CurrentBlock.GetComponent<Rigidbody2D>().gravityScale = gravityScale;
     }
 
 
-    //function to check if the provided row and column indeces reference a valid block
-    bool BlockPresent(int row, int col)
-    {
-        //if ((row < 0) || (col < 0)) return false;
-        //if ((row >= Column.MAX_NUM_ROWS) || (col >= NUM_COLS)) return false;
-        //return (row < Columns[col].Count);
-        return false;
-    }
 
     List<GameObject> GetColumn(int col)
     {
@@ -245,8 +223,8 @@ public class Board : MonoBehaviour
         
         blocks.Add(block);
 
-        Debug.Log(block.name);
-        Debug.Log("SN: " + block.GetComponent<BlockController>().SerialNumber);
+        //Debug.Log(block.name);
+        //Debug.Log("SN: " + block.GetComponent<BlockController>().SerialNumber);
 
         //now follow the linked blocks
         while (block.GetComponent<BlockController>().TriggerTop.GetComponent<ProximitySensor>().Objects.Count > 0)
@@ -260,6 +238,9 @@ public class Board : MonoBehaviour
         return blocks;
     }
 
+    /// <summary>
+    /// Function to build the board matrix based on the current position of blocks on the board.
+    /// </summary>
     void BuildBoardMatrix()
     {
         for (int col = 0; col < NumColumns; col++)
@@ -274,6 +255,229 @@ public class Board : MonoBehaviour
             }
         }
     }
+
+    /// <summary>
+    /// Function to return the BlockController component of the block
+    /// game objects in the BoardMatrix.  The function assumes that
+    /// the matrix is up-to-date and that the provided indeces are correct.
+    /// </summary>
+    /// <param name="row">The matrix row</param>
+    /// <param name="col">The matrix column</param>
+    /// <returns></returns>
+    BlockController BCMatrix(int col, int row)
+    {
+        return BoardMatrix[col, row].GetComponent<BlockController>();
+    }
+
+
+
+    //function to check if the provided row and column indeces reference a valid block
+      bool BlockPresent(int col, int row)
+    {
+        if ((row < 0) || (col < 0)) return false;
+        if ((row >= MAX_NUM_ROWS) || (col >= NumColumns)) return false;
+        return (BoardMatrix[col, row] != null);
+    }
+
+    //function to check if 2 blocks can be merged (same power)
+    //CanMerge() assumes the indeces are valid
+    bool CanMerge(int col0, int row0, int col1, int row1)
+    {
+        return (BCMatrix(col0, row0).Power == BCMatrix(col1, row1).Power);
+    }
+
+
+    //overloaded function to merge 4 blocks.  Merge() verifies valid indices
+    //and matching powers before attempting to merge.  Returns true on success
+    //false otherwise.
+    bool Merge(int col0, int row0, int col1, int row1, int col2, int row2, int col3, int row3)
+    {
+        //verify all indices are valid
+        if (!BlockPresent(col0, row0)) return false;
+        if (!BlockPresent(col1, row1)) return false;
+        if (!BlockPresent(col2, row2)) return false;
+        if (!BlockPresent(col3, row3)) return false;
+
+        //verify 3-way merge
+        if (!CanMerge(col0, row0, col1, row1)) return false;
+        if (!CanMerge(col0, row0, col2, row2)) return false;
+        if (!CanMerge(col0, row0, col3, row3)) return false;
+
+        Debug.Log("Merge3");
+
+        //increment power by 3
+        BCMatrix(col0, row0).Power += 3;
+
+        //delete 3 blocks
+        BCMatrix(col1, row1).Terminate(GetSlideDirection(col0, row0, col1, row1));
+        BCMatrix(col2, row2).Terminate(GetSlideDirection(col0, row0, col2, row2));
+        BCMatrix(col3, row3).Terminate(GetSlideDirection(col0, row0, col3, row3));
+
+        PrintBoard();
+
+        return true;
+    }
+
+
+    //overloaded function to merge 3 blocks.  Merge() verifies valid indices
+    //and matching powers before attempting to merge.  Returns true on success
+    //false otherwise.
+    bool Merge(int col0, int row0, int col1, int row1, int col2, int row2)
+    {
+        //verify all indices are valid
+        if (!BlockPresent(col0, row0)) return false;
+        if (!BlockPresent(col1, row1)) return false;
+        if (!BlockPresent(col2, row2)) return false;
+
+        //verify 2-way merge
+        if (!CanMerge(col0, row0, col1, row1)) return false;
+        if (!CanMerge(col0, row0, col2, row2)) return false;
+
+        Debug.Log("Merge2");
+
+        //increment power by 2
+        BCMatrix(col0, row0).Power += 2;
+
+        //delete 2 blocks
+        BCMatrix(col1, row1).Terminate(GetSlideDirection(col0, row0, col1, row1));
+        BCMatrix(col2, row2).Terminate(GetSlideDirection(col0, row0, col2, row2));
+
+        PrintBoard();
+
+        return true;
+    }
+
+    //overloaded function to merge 2 blocks.  Merge() verifies valid indices
+    //and matching powers before attempting to merge.  Returns true on success
+    //false otherwise.
+    bool Merge(int col0, int row0, int col1, int row1)
+    {
+        //verify all indices are valid
+        if (!BlockPresent(col0, row0)) return false;
+        if (!BlockPresent(col1, row1)) return false;
+
+        //verify 1-way merge
+        if (!CanMerge(col0, row0, col1, row1)) return false;
+
+        Debug.Log("Merge1");
+
+        //increment power by 1
+        BCMatrix(col0, row0).Power += 1;
+
+        //delete 1 block
+        BCMatrix(col1, row1).Terminate(GetSlideDirection(col0, row0, col1, row1));
+
+        PrintBoard();
+
+        return true;
+    }
+
+
+
+    BlockController.Slide GetSlideDirection(int col0, int row0, int col1, int row1)
+    {
+        if (col0 > col1) return BlockController.Slide.RIGHT;
+        if (col1 > col0) return BlockController.Slide.LEFT;
+        if (row0 > row1) return BlockController.Slide.UP;
+        if (row0 < row1) return BlockController.Slide.DOWN;
+
+        Debug.Log("GetSlideDirection(): No valid slide direction found.");
+        return BlockController.Slide.NONE;
+    }
+
+    //function to find mergeable blocks
+    void FindMergeables()
+    {
+        bool FoundMatch;
+
+        //check for 3 neighbors (L+R+B)
+        for (int row = MAX_NUM_ROWS - 1; row >= 1; row--)
+        {
+            for (int col = 1; col < NumColumns-1; col++)
+            {
+                //if (row >= Columns[col].Count) continue;
+
+                FoundMatch = Merge(col, row, col - 1, row, col + 1, row, col, row - 1);
+                if (FoundMatch)
+                {
+                    Debug.Log("[LRB]");
+                    return;
+                }
+            }  //for (int col = ...
+        } //for (int row = ...
+
+
+        //check for 2 neighbors (L+R), (R+B) or (L+B)
+        FoundMatch = false;
+        for (int row = MAX_NUM_ROWS - 1; row >= 0; row--)
+        {
+            for (int col = 1; col < NumColumns-1; col++)
+            {
+                //if (row >= Columns[col].Count) continue;
+
+                FoundMatch = Merge(col, row, col - 1, row, col + 1, row);
+                if (FoundMatch)
+                {
+                    Debug.Log("[LR]");
+                    return;
+                }
+
+                FoundMatch = Merge(col, row, col + 1, row, col, row - 1);
+                if (FoundMatch)
+                {
+                    Debug.Log("[RB]");
+                    return;
+                }
+
+                FoundMatch = Merge(col, row, col - 1, row, col, row - 1);
+                if (FoundMatch)
+                {
+                    Debug.Log("[LB]");
+                    return;
+                }
+            }  //for (int col = ...
+        } //for (int row = ...
+
+
+
+
+        //check for 1 neighbor (L), (R) or (B).
+        for (int row = MAX_NUM_ROWS - 1; row >= 0; row--)
+        {
+            for (int col = 0; col < NumColumns; col++)
+            {
+                //if (row >= Columns[col].Count) continue;
+
+                FoundMatch = Merge(col, row, col - 1, row);
+                if (FoundMatch)
+                {
+                    Debug.Log("[L]");
+                    return;
+                }
+
+                FoundMatch = Merge(col, row, col + 1, row);
+                if (FoundMatch)
+                {
+                    Debug.Log("[R]");
+                    return;
+                }
+
+                FoundMatch = Merge(col, row, col, row - 1);
+                if (FoundMatch)
+                {
+                    Debug.Log("[B]");
+                    return;
+                }
+            }  //for (int col = ...
+        } //for (int row = ...
+
+    }   //void FindMergeables()
+
+
+
+
+
+
 
 
 
